b7842c9f72fe4cf6c2b5c00e9992a35e54c2ed00
Pamela Candiani <pamela.candianipcs@gmail.com>
2024-12-07 19:05:57 -0300

    Criando a entidade

---
+++ src/main/java/br/fatec/agendadecompromisso/entities/Agenda.java
@@ -0,0 +1,84 @@
+package br.fatec.agendadecompromisso.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Agenda implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String compromisso;
+    private String date;
+    private String time;
+    private String local;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompromisso() {
+        return compromisso;
+    }
+    public void setCompromisso(String compromisso) {
+        this.compromisso = compromisso;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Agenda other = (Agenda) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+    
+
+
+}

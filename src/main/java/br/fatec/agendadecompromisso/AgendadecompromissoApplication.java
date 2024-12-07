584281749cc92ecfb25ff9c5c2f7db506b90bcd8
Pamela Candiani <pamela.candianipcs@gmail.com>
2024-12-07 18:33:35 -0300

    Inicio do Projeto Agenda de Compromissos (backend) com as dependencias

---
+++ src/main/java/br/fatec/agendadecompromisso/AgendadecompromissoApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.agendadecompromisso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadecompromissoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadecompromissoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/agendadecompromisso/AgendadecompromissoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.agendadecompromisso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadecompromissoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

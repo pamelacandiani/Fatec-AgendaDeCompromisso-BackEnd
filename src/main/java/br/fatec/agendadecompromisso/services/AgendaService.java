cb40b5676ea82ab8c5d4e1f97de53d12485aa985
Pamela Candiani <pamela.candianipcs@gmail.com>
2024-12-09 16:03:15 -0300

    Criando end-point get por id

--- src/main/java/br/fatec/agendadecompromisso/services/AgendaService.java
+++ src/main/java/br/fatec/agendadecompromisso/services/AgendaService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.fatec.agendadecompromisso.dto.AgendaResponse;
+import br.fatec.agendadecompromisso.entities.Agenda;
 import br.fatec.agendadecompromisso.mappers.AgendaMapper;
 import br.fatec.agendadecompromisso.repositories.AgendaRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AgendaService {
@@ -20,5 +22,12 @@ public class AgendaService {
         return repository.findAll().stream().map( a -> AgendaMapper.toDTO(a)).collect(Collectors.toList());
     }
 
+    public AgendaResponse getAgendaById(Long id){
+        Agenda agenda = repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Compromisso não registrado")
+        );
+        
+        return AgendaMapper.toDTO(agenda);
+    }
 
 }
